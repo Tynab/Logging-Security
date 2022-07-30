eb59c59a3a9b731641ddf1c1792fa6233bf066c0
Yami An <yamiannephilim@gmail.com>
2022-07-30 11:56:33 +0700

    yan

--- bai_tap_nop/src/main/java/com/yan/bai_tap_nop/controller/HelloController.java
+++ bai_tap_nop/src/main/java/com/yan/bai_tap_nop/controller/HelloController.java
@@ -7,15 +7,15 @@ import static org.slf4j.LoggerFactory.*;
 
 @RestController
 public class HelloController {
-    private Logger logger = getLogger(HelloController.class);
+    private Logger mLogger = getLogger(HelloController.class);
 
     @RequestMapping("/hello")
     public String hello() {
-        logger.trace("A Trace message");
-        logger.debug("A Debug message");
-        logger.info("A Info message");
-        logger.warn("A Warning message");
-        logger.error("A Error message");
+        mLogger.trace("A Trace message");
+        mLogger.debug("A Debug message");
+        mLogger.info("A Info message");
+        mLogger.warn("A Warning message");
+        mLogger.error("A Error message");
         return "Hello World";
     }
 }
